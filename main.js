6f9d59b10a1e8e85421df9fa6471d38c4bbe9cc9
RickyAnchores <ricky23anchores@gmail.com>
2022-09-11 14:52:29 +0100

    update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-window.addEventListener('load',() => {
+// for automated geolocalisation 
+/*window.addEventListener('load',() => {
     let long;
     let lat;
 
@@ -11,4 +12,34 @@ window.addEventListener('load',() => {
     }else{
         h1.textContent = "hey this is not working because your not allowing GeoLocation"
     }
-});
+}); */
+
+
+
+// WITH INPUT CITY
+
+
+
+let button = document.querySelector('.btn');
+let inputValue = document.querySelector('.inputValue');
+let name = document.querySelector('.location-name');
+let temperature = document.querySelector('.temperature-degree');
+let description = document.querySelector('.temperature-description');
+
+
+button.addEventListener('click', () => {
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=62d768686c18a4ed0026aaefe7330c87')
+    .then(response => response.json())
+    .then(data => {
+        let nameValue = data['name'];
+        let tempValue = data['main']['temp'];
+        let descriptionValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temperature.innerHTML = tempValue;
+        description.innerHTML = descriptionValue;
+    })
+    .catch(err => alert("Wrong CITY"))
+})
+
